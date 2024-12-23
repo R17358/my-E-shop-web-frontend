955a32a0bcf85858cea47d0de5736c1c6a4dba3b
Ritesh <shivay.lfc@gmail.com>
2024-12-23 16:03:26 +0530

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 const instance = axios.create({
-  baseURL: 'https://my-e-shop-web-backend.onrender.com/api/v1', //http://localhost:4000
+  baseURL: 'https://my-e-shop-web-backend.onrender.com', //http://localhost:4000
   withCredentials: true,
   
 });
